6306fb059e37c0fc25773b0c7dd848bd4e4aa163
Chiyo Mochizuki <Chiyo Mochizuki@DESKTOP-L1VT2TP>
2023-07-04 17:20:30 +0300

    Read

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+    @Test
+    public void testWhenFewSquaresFound() {
+        SQRService service = new SQRService();
+
+        int actual = service.calc(200,300);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
